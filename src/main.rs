6c76f913c9a0d660dcce0cdfe0e36e785a69d4e6
Anuta-loginova <anna.loginova.0219@gmail.com>
2025-08-05 17:25:07 +0300

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+use std::sync::{Arc, Mutex};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Device, Stream, StreamConfig, SupportedStreamConfig};
+use anyhow::Result;
+
+
+/// Инициализация аудиоустройства и конфигурации
+fn init_audio_device() -> Result<(Device, SupportedStreamConfig)> {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .ok_or_else(|| anyhow::anyhow!("Нет выходного аудиоустройства"))?;
+
+    let config = device.default_output_config()?;
+    Ok((device, config))
+}
+
 fn main() {
-  println!("Hello, world!");
+
 }
